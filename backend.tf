13793e5edef855571d0638ea7814cc88b2e07036
ahmedovelshan <80003705+ahmedovelshan@users.noreply.github.com>
2024-08-22 18:29:00 +0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -7,4 +7,10 @@ terraform {
     encrypt        = true
   }
   required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
 }
